0be0cb256261ea4011d1a5785b5280f5cd301e34
Altoid <102745236+Raynnerkavulika@users.noreply.github.com>
2024-11-29 08:26:20 +0300

    Add files via upload

--- config.php
+++ config.php
@@ -1,7 +1,17 @@
 <?php
-$dbname = 'mysql:host=localhost;dbname=coffee_shop';
+$dbname = 'mysql:host=localhost;dbname=real_estate';
 $username = 'root';
 $password = '';
 
 $conn = new PDO($dbname,$username,$password);
+
+function create_unique_id(){
+    $char = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $char_len = strlen($char);
+    $rand_str = '';
+    for($i = 0; $i < 20; $i++){
+        $rand_str .= $char[mt_rand(0, $char_len - 1)];
+    }
+    return $rand_str;
+}
 
